0fe7ebf5f7696607d7918f1c7ea79779b58e61bb
Devlin Duldulao <webmasterdevlin@gmail.com>
2021-02-14 20:30:14 +0100

    updated

--- src/app/ngxs/services/hero.service.ts
+++ src/app/ngxs/services/hero.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Hero } from "../../features/hero/hero.model";
-import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class HeroService {
--- src/app/ngxs/services/villain.service.ts
+++ src/app/ngxs/services/villain.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
+import { environment } from "../../../environments/environment";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { Villain } from "../../features/villain/villain.model";
-import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class VillainService {
